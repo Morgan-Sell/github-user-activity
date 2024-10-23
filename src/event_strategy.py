ef90e6c251c7b32102ae6100cf83259c1598c894
Morgan-Sell <morganpsell@gmail.com>
2024-10-22 18:45:30 -0700

    small refactor of event_strategy.py classes

--- src/event_strategy.py
+++ src/event_strategy.py
@@ -2,56 +2,112 @@ from abc import ABC, abstractmethod
 
 
 class EventExtractionStrategy(ABC):
-    def extract_event_id(self, event_data):
+    """
+    Abstract base class for extracting event details from event data.
+    """
+    def extract_event_id(self, event_data) -> str:
+        """
+        Extract the event ID from the event data.
+
+        Args:
+            event_data (dict): The event data containing event details.
+
+        Returns:
+            str: The event ID.
+        """
         return event_data.get("id")
 
-    def extract_repo_name(self, event_data):
+    def extract_repo_name(self, event_data) -> str:
+        """
+        Extract the repository name from the event data.
+
+        Args:
+            event_data (dict): The event data containing repository details.
+
+        Returns:
+            str: The repository name.
+        """
         return event_data["repo"]["name"]
 
     @abstractmethod
-    def extract_title(self, event_data):
+    def extract_title(self, event_data) -> str:
+        """
+        Abstract method to extract the event title.
+
+        Args:
+            event_data (dict): The event data containing the event title.
+
+        Returns:
+            str: The event title.
+        """
         pass
+
+    def extract_action(self, event_data) -> str:
+        """
+        Abstract method to extract the event action.
+
+        Args:
+            event_data (dict): The event data containing the event action.
+
+        Returns:
+            str: The event action.
+        """
+        return event_data["payload"]["action"]
 
     @abstractmethod
-    def extract_action(self, event_data):
+    def extract_recipient(self, event_data) -> str:
+        """
+        Abstract method to extract the event recipient.
+
+        Args:
+            event_data (dict): The event data containing the recipient details.
+
+        Returns:
+            str: The recipient of the event.
+        """
         pass
 
-    @abstractmethod
-    def extract_recipient(self, event_data):
-        pass
+    def extract_created_date(self, event_data) -> str:
+        """
+        Extract the event creation date.
 
-    def extract_created_date(self, event_data):
+        Args:
+            event_data (dict): The event data containing the creation date.
+
+        Returns:
+            str: The event creation date in ISO format.
+        """
         return event_data.get("created_at")
 
 
 class IssueCommentStrategy(EventExtractionStrategy):
-    def extract_title(self, event_data):
+    """
+    Strategy for extracting details from IssueCommentEvent data.
+    """
+    def extract_title(self, event_data) -> str:
         return event_data["payload"]["issue"]["title"]
 
-    def extract_action(self, event_data):
-        return event_data["payload"]["action"]
-
-    def extract_recipient(self, event_data):
+    def extract_recipient(self, event_data) -> str:
         return event_data["payload"]["issue"]["user"]["login"]
 
 
 class IssueStrategy(EventExtractionStrategy):
+    """
+    Strategy for extracting details from IssuesEvent data.
+    """
     def extract_title(self, event_data) -> str:
         return event_data["payload"]["issue"]["title"]
-
-    def extract_action(self, event_data) -> str:
-        return event_data["payload"]["action"]
 
     def extract_recipient(self, event_data) -> str:
         return event_data["payload"]["issue"]["user"]["login"]
 
 
 class PullRequestStrategy(EventExtractionStrategy):
+    """
+    Strategy for extracting details from PullRequestEvent data.
+    """
     def extract_title(self, event_data) -> str:
         return event_data["payload"]["pull_request"]["title"]
-
-    def extract_action(self, event_data) -> str:
-        return event_data["payload"]["action"]
 
     def extract_recipient(self, event_data) -> str:
         try:
@@ -61,17 +117,20 @@ class PullRequestStrategy(EventExtractionStrategy):
 
 
 class PullRequestReviewStrategy(EventExtractionStrategy):
+    """
+    Strategy for extracting details from PullRequestReviewEvent data.
+    """
     def extract_title(self, event_data) -> str:
         return event_data["payload"]["pull_request"]["title"]
-
-    def extract_action(self, event_data) -> str:
-        return event_data["payload"]["action"]
 
     def extract_recipient(self, event_data) -> str:
         return event_data["payload"]["pull_request"]["user"]["login"]
 
 
 class PushStrategy(EventExtractionStrategy):
+    """
+    Strategy for extracting details from PushEvent data.
+    """
     def extract_title(self, event_data) -> str:
         return event_data["payload"]["commits"][0]["message"]
 
